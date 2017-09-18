df5eb85cb8b15ae350683e0b70745effb5a3f255
Dang <hadangktmt@gmail.com>
2017-09-18 14:43:01 +0700

    update xu ly ky tu dac biet (dau cham, dau phay)

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ function reverseString (str) {
 	var strBuff = str.split(" ");
 	console.log(strBuff);
 	var strReverse = "";
+	var strBuffSpecialCharacter;
+	var specialCharacter;
 	for (var i = 0; i < strBuff.length; i++) {
-		
-			strBuff[i] = strBuff[i].split("").reverse().join("");
-		
-		
+			specialCharacter = strBuff[i].charAt(strBuff[i].length - 1);
+			if( [".", ","].indexOf(specialCharacter) > -1 ) {
+				
+				console.log(specialCharacter);
+				// console.log(strBuff[4].charAt(strBuff.length - 1));
+				strBuffSpecialCharacter = strBuff[i].slice(0, strBuff[i].length - 1);
+				console.log(strBuffSpecialCharacter);
+				strBuffSpecialCharacter = strBuffSpecialCharacter.split("").reverse().join("");
+				strBuff[i] = strBuffSpecialCharacter + specialCharacter;
+			} else {
+				strBuff[i] = strBuff[i].split("").reverse().join("");
+			}
+				
 	}
 	strReverse = strBuff.join(" ");
 	return strReverse;
@@ -44,6 +55,6 @@ function searchMaxWord (str) {
 
 //test case
 
-console.log(reverseString("hi there, i am Dang"));
+console.log(reverseString("hi there. i am Dang. ?!"));
 console.log(upperFirstLetter("hi there, i am Dang"));
-console.log(searchMaxWord("hi thereeeeeeeeeeee, i am Daaaaaaaaaaaaaaaaang"));
+console.log(searchMaxWord("hi thereeeeeeeeeeee, i am Daaaaaaaaaaaaaaaaang ?????????????????????????????????????????????????!!!!!!!!!!!!"));
