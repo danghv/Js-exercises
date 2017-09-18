446f34864de3e7b5db7d1e73e1fef7d2b005d59c
Dang <hadangktmt@gmail.com>
2017-09-18 17:29:23 +0700

    up

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 // ham dao chuoi
-function reverseString (str) {
+function reverseString(str) {
 	var strBuff = str.split(" ");
 	// console.log(strBuff);
 	var strReverse = "";
 	var strBuffSpecialCharacter;
 	var specialCharacter;
 	for (var i = 0; i < strBuff.length; i++) {
-			specialCharacter = strBuff[i].charAt(strBuff[i].length - 1);
-			if( [".", ",", "?"].indexOf(specialCharacter) > -1 ) {
-				
-				// console.log(specialCharacter);
-				// console.log(strBuff[4].charAt(strBuff.length - 1));
-				strBuffSpecialCharacter = strBuff[i].slice(0, strBuff[i].length - 1);
-				// console.log(strBuffSpecialCharacter);
-				strBuffSpecialCharacter = strBuffSpecialCharacter.split("").reverse().join("");
-				strBuff[i] = strBuffSpecialCharacter + specialCharacter;
-			} else {
-				strBuff[i] = strBuff[i].split("").reverse().join("");
-			}
-				
+		specialCharacter = strBuff[i].charAt(strBuff[i].length - 1);
+		if ([".", ",", "?"].indexOf(specialCharacter) > -1) {
+
+			// console.log(specialCharacter);
+			// console.log(strBuff[4].charAt(strBuff.length - 1));
+			strBuffSpecialCharacter = strBuff[i].slice(0, strBuff[i].length - 1);
+			// console.log(strBuffSpecialCharacter);
+			strBuffSpecialCharacter = strBuffSpecialCharacter.split("").reverse().join("");
+			strBuff[i] = strBuffSpecialCharacter + specialCharacter;
+		} else {
+			strBuff[i] = strBuff[i].split("").reverse().join("");
+		}
+
 	}
 	strReverse = strBuff.join(" ");
 	return strReverse;
 }
 //ham check cu phap string
-function checkString (str) {
+function checkString(str) {
 	var specialCharacters = ['.', ',', '?', ';', ':'];
 	for (var index = 0; index < specialCharacters.length; index++) {
 		// console.log(str.indexOf(specialCharacters[index]));
-		if(str.indexOf(specialCharacters[index]) > -1) {
+		if (str.indexOf(specialCharacters[index]) > -1) {
 			return false;
 		}
-		
+
 	}
 	return true;
 }
 // ham upper letter
-function upperFirstLetter (str) {
+function upperFirstLetter(str) {
 	var strBuff = str.split(" ");
 	var buff;
 	var finalCharacterOfWord;
@@ -49,48 +49,60 @@ function upperFirstLetter (str) {
 		finalCharacterOfWord = strBuff[i].charAt(strBuff[i].length - 1);
 		// console.log(finalCharacterOfWord);
 		// console.log(specialCharacters.indexOf(finalCharacterOfWord));
-		if(checkString(strBuff[i]) === false && specialCharacters.indexOf(finalCharacterOfWord) === -1) {
+		if (checkString(strBuff[i]) === false && specialCharacters.indexOf(finalCharacterOfWord) === -1) {
 			// strBuff[i].replace(/\./g, " ");
 			buff = strBuff[i].replace(/\.|\:|\;|\,/g, " ").split(" ");
 			// console.log(buff);
-			for( var j = 0; j < buff.length; j++) {
+			for (var j = 0; j < buff.length; j++) {
 				buff[j] = buff[j].charAt(0).toUpperCase() + buff[j].slice(1);
 			}
-			
+
 			strBuff[i] = buff.join(" ");
 			console.log(strBuff[i]);
-			
-			
-		}else{
+
+
+		} else {
 			strBuff[i] = strBuff[i].charAt(0).toUpperCase() + strBuff[i].slice(1);
 			// console.log(strBuff[i]);
 		}
 		// var buff = strBuff[i].split(" ");
 		// result += " " + strBuff[i].charAt(0).toUpperCase() + strBuff[i].slice(1);
-		
-		
+
+
 	}
-	
+
 	return strBuff.join(" ");
 
 }
 // search the max strength word
-function searchMaxWord (str) {
-	var strBuff = str.replace(/,/g, "").split(" ");
-	// console.log(strBuff);
-	var maxWord = ""; 
+function searchMaxWord(str) {
+	var strBuff = str.split(" ");
+	var maxWord = "";
+	var buff;
 	for (var i = 0; i < strBuff.length; i++) {
-		if(strBuff[i].length > maxWord.length) {
-			maxWord = strBuff[i];
-			// console.log(maxWord);
+		if (checkString(strBuff[i]) === false) {
+			buff = strBuff[i].replace(/\.|\,|\:|\;/g, " ").split(" ");
+			// console.log(buff);
+			for (var j = 0; j < buff.length; j++) {
+				if (buff[j].length > maxWord.length) {
+					maxWord = buff[j];
+				}
+				// console.log(maxWord);
+
+			}
+		} else {
+			if (strBuff[i].length > maxWord.length) {
+				maxWord = strBuff[i];
+				// console.log(maxWord);
+			}
 		}
-		
 	}
+
 	return maxWord;
 }
 
 //test case
 
-console.log(reverseString("Một tiếng súng chỉ thiên vang lên uy hiếp Thọ, trinh sát P. lao vào đập tan cửa kính taxi, khi những mảnh kính rơi còn chưa kịp chạm đất thì cánh tay chắc nịch của trinh sát đã khống chế Thọ."));
-console.log(upperFirstLetter("hi. there, i am Dang.good,to:see;you"));
-console.log(searchMaxWord("hi thereeeeeeeeeeee, i am Daaaaaaaaaaaaaaaaang ?????????????????????????????????????????????????!!!!!!!!!!!!"));
+// console.log(reverseString("Một tiếng súng chỉ thiên vang lên uy hiếp Thọ, trinh sát P. lao vào đập tan cửa kính taxi, khi những mảnh kính rơi còn chưa kịp chạm đất thì cánh tay chắc nịch của trinh sát đã khống chế Thọ."));
+// console.log(upperFirstLetter("hi. there, i am Dang.good,to:see;you"));
+console.log(searchMaxWord("hi 1 12 123 1234 thereee, minh.la.abcdefghijklmnmmmmm test"));
