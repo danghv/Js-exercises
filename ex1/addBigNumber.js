b87afa313094b2c49c42f6c4523b5f848eec241f
danghv <hadangktmt@gmail.com>
2017-09-26 00:37:10 +0700

    update big number

--- ex1/addBigNumber.js
+++ ex1/addBigNumber.js
@@ -29,7 +29,24 @@ function addBigNumber(number1, number2) {
     var i, j;
     var n1 = new Stack();
     var n2 = new Stack();
-    for (var i = 0; i < number1.length; i++) {
-
+    var result = new Stack();
+    var a = number1.toString().split('');
+    var b = number2.toString().split('');
+    var sub, carry;
+    for (var i = 0; i < a.length; i++) {
+        n1.pushStack(a[i]);
     }
+    console.log(n1.dataStore);
+    for (var j = 0; j< b.length; j++) {
+        n2.pushStack(b[j]);
+    }
+    console.log(n2.dataStore);
+    if (n1.lengthStack() < n2.lengthStack()) {
+        while (n2.lengthStack() > 0) {
+            sub = n1.popStack() + n2.popStack();
+        }
+    }
+
 }
+
+console.log(addBigNumber(389, 429));
